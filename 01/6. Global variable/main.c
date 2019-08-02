e0a1ccc26709568f182341defc3448adf8acbdc7
Hayden\hyouk <hayden.hur@pocu.academy>
2019-08-02 02:43:16 -0700

    Modified global variable example

--- 01/6. Global variable/main.c
+++ 01/6. Global variable/main.c
@@ -7,13 +7,14 @@ void IncreaseNumber()
 	int y = 200;
 	y += 100;
 	x += 100;
+	
+	printf("%d\n", y);
 }
 
 int main()
 {
 	printf("%d\n", x);
 	IncreaseNumber();
-	printf("%d\n", y);
 	printf("%d\n", x);
 	return 0;
 }
