9bbdec7753df1a67cb33c463feb1a9915f0055f3
Hayden\hyouk <hayden.hur@pocu.academy>
2019-08-04 01:39:50 -0700

    Modified coding style of switch staement example

--- 01/5. Switch statement/main.c
+++ 01/5. Switch statement/main.c
@@ -4,16 +4,16 @@ int main(void)
 {
     int x = 3;
     switch (x) {
-        case 0:
-            printf("Case 0\n");
-        case 1:
-            printf("Case 1\n");
-        case 2:
-            printf("Case 2\n");
-        case 3:
-            printf("Case 3\n");
-        default:
-            printf("default case\n");
+    case 0:
+        printf("Case 0\n");
+    case 1:
+        printf("Case 1\n");
+    case 2:
+        printf("Case 2\n");
+    case 3:
+        printf("Case 3\n");
+    default:
+        printf("default case\n");
     }
 
     return 0;
