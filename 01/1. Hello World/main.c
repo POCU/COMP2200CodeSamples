7b9467c5561cace5aa1337590a2b7e28e741c42a
Hayden\hyouk <hayden.hur@pocu.academy>
2019-08-05 03:56:36 -0700

    Made subtle changes to codes

--- 01/1. Hello World/main.c
+++ 01/1. Hello World/main.c
@@ -3,5 +3,6 @@
 int main(void)
 {
     printf("Hello, World!\n");
-    return 0;
+    
+	return 0;
 }
--- 01/5. Switch statement/main.c
+++ 01/5. Switch statement/main.c
@@ -6,10 +6,13 @@ int main(void)
     switch (x) {
     case 0:
         printf("Case 0\n");
+		break;
     case 1:
         printf("Case 1\n");
+		break;
     case 2:
         printf("Case 2\n");
+		break;
     case 3:
         printf("Case 3\n");
     default:
