3c1a51f34c7380c810be0380514309ddbfa68526
Pope Kim <popekim@users.noreply.github.com>
2019-08-10 11:54:36 -0700

    added comment for uncompilable commented-out code

--- 01/6_global_variable/main.c
+++ 01/6_global_variable/main.c
@@ -11,9 +11,11 @@ void increase_number(void)
 
 int main(void)
 {
-    printf("g_x: %d\n", g_x);
+    printf("g_x: %d\n", g_x); /* 100 */
     increase_number();
-    printf("g_x: %d\n", g_x);
+    printf("g_x: %d\n", g_x); /* 101 */
+    /* below does not compile */
     /*printf("y: %d\n", y);*/
+
     return 0;
 }
