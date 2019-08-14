da6087207cdde562c39e91978c0bd84148664310
hayden.hur <hayden.hur@pocu.academy>
2019-08-14 19:26:26 +0900

    Fixed static keyword example a bit

--- 02/6_static_keyword/main.c
+++ 02/6_static_keyword/main.c
@@ -4,7 +4,7 @@
 
 extern unsigned int g_chequing;
 extern unsigned int g_saving;
-extern int s_fee;
+extern unsigned int s_fee;
 extern void set_fee(const unsigned int fee);
 
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
     printf("g_chequing: %u\n", g_chequing);     /* 0 */
     printf("s_saving: %u\n", g_saving);         /* 0 */
     /* this is a linker error */
-    /*printf("s_fee: %d", s_fee);*/             /* 20 */
+    /*printf("s_fee: %u", s_fee);*/             /* 20 */
 
     deposit_into_chequing(300u);
     deposit_into_saving(700u);
@@ -22,7 +22,7 @@ int main(void)
     printf("g_chequing: %u\n", g_chequing);     /* 280 */
     printf("s_saving: %u\n", g_saving);         /* 680 */
     /* this a linker error */
-    /*printf("s_fee: %d", s_fee);*/             /* 100 */
+    /*printf("s_fee: %u", s_fee);*/             /* 100 */
 
     return 0;
 }
