54eb692a4b4931adfcc286963d1481d161c64323
Pope Kim <popekim@gmail.com>
2019-09-11 12:19:21 -0700

    04: - stricmp sample shows string matching example between lower and uppercases - compacted output message

--- 04/1_string_case_insensitive_compare/main.c
+++ 04/1_string_case_insensitive_compare/main.c
@@ -7,13 +7,13 @@ int main(void)
     int diff;
 
     diff = string_case_insensitive_compare("hello", "HELL");
-    printf("hello compared to HELL: %d\n", diff);    /* 1 */
+    printf("hello to HELL: %d\n", diff);   /* 1 */
 
     diff = string_case_insensitive_compare("hello", "yellow");
-    printf("hello compared to yellow: %d\n", diff);  /* -1 */
+    printf("hello to yellow: %d\n", diff); /* -1 */
 
-    diff = string_case_insensitive_compare("hello", "hello");
-    printf("hello compared to hello: %d\n", diff);   /* 0 */
+    diff = string_case_insensitive_compare("hello", "HELLO");
+    printf("hello to HELLO: %d\n", diff);  /* 0 */
 
     return 0;
 }
