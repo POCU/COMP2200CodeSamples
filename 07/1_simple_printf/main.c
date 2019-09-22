d14485a818ef9b801f6dfc6b3ad22f86de810958
Pope Kim <popekim@gmail.com>
2019-09-21 22:52:01 -0700

    07: print formatting change

--- 07/1_simple_printf/main.c
+++ 07/1_simple_printf/main.c
@@ -4,9 +4,10 @@ int main(void)
 {
     printf_simple("c\n", 'A');
     printf_simple("d\n", 10);
-    printf_simple("s\n", "Hello\n");
+    printf_simple("s\n", "Hello");
     
-    printf_simple("c d s", 'A', 10, "Hello\n");
+    printf_simple("\n");
+    printf_simple("c d s\n", 'A', 10, "Hello");
 
     return 0;
 }
