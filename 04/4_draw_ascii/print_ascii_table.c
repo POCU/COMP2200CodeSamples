c753946ef792f2b4a168dcfd1d6ca1f46b9f2ad5
Hayden Hur <hayden.hur@pocu.academy>
2019-10-13 06:33:22 -0700

    Fixed draw_ascii code sample without adding more codes

--- 04/4_draw_ascii/print_ascii_table.c
+++ 04/4_draw_ascii/print_ascii_table.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
+#include <limits.h>
 #include "print_ascii_table.h"
 
 void print_ascii_table(void)
 {
     const char MIN_PRINTABLE_ASCII = 32;
-    const char MAX_PRINTABLE_ASCII = 126;
     const char NUM_ROWS = 32;
     const char COL_CHAR_OFFSET = 32;
 
@@ -23,8 +22,6 @@ void print_ascii_table(void)
         printf("%03d %#X %c\t", ch, ch, ch);
 
         ch += COL_CHAR_OFFSET;
-        if (ch <= MAX_PRINTABLE_ASCII) {
-            printf("%03d %#X %c\n", ch, ch, ch);
-        }
+        ch < SCHAR_MAX ? printf("%03d %#X %c\n", ch, ch, ch) : printf("");
     }
 }
