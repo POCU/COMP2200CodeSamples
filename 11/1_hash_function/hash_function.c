d2021b729cb51facba2cd89d025504ba008ae77d
Pope Kim <popekim@gmail.com>
2019-11-06 18:49:52 -0800

    fixed tab and whitespace difference

--- 11/1_hash_function/hash_function.c
+++ 11/1_hash_function/hash_function.c
@@ -15,14 +15,14 @@ size_t hash_float(float value)
 size_t hash_data(const void* data, size_t length)
 {
     const char* p;
-	size_t i;
-	size_t hash;
+    size_t i;
+    size_t hash;
 
     p = data;
-	hash = 0;
-	for (i = 0; i < length; ++i) {
-		hash = 65599 * hash + *p++;
-	}
+    hash = 0;
+    for (i = 0; i < length; ++i) {
+        hash = 65599 * hash + *p++;
+    }
 
-	return hash ^ (hash >> 16);
+    return hash ^ (hash >> 16);
 }
