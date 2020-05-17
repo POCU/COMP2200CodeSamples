d6ca14e759b48aa2e2e2496442f7d9003752f5e6
monkeywithbanana <>
2020-05-16 17:22:03 -0700

    week7 - fix a bug where simple_printf() does not print 0

--- 07/1_simple_printf/simpleio.c
+++ 07/1_simple_printf/simpleio.c
@@ -4,13 +4,17 @@
 
 #include "simpleio.h"
 
-static void print_int_recursive(unsigned int val)
+static void print_int_recursive(unsigned int val, unsigned int depth)
 {
-    if (val == 0) {
-        return;
-    }
+	if (val == 0 && depth == 0) {
+		putchar('0');
+	}
 
-    print_int_recursive(val / 10);
+	if (val == 0) {
+		return;
+	}
+
+    print_int_recursive(val / 10, depth + 1);
     putchar('0' + val % 10);
 }
 
@@ -37,7 +41,7 @@ void printf_simple(const char* format, ...)
                     break;
                 case 'd':
                     val = va_arg(ap, unsigned int);
-                    print_int_recursive(val);
+                    print_int_recursive(val, 0);
                     break;
                 default:
                     putchar(*format);
